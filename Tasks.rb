c04c8d17442b4c2c644463f83ac548b3735aac27
MShajieShah <63161585+MShajieShah@users.noreply.github.com>
2020-04-05 00:24:44 +0500

    Add files via upload

--- Tasks.rb
+++ Tasks.rb
@@ -85,16 +85,78 @@
 #Ruby_Blanks
 
 
-ss = ['verb','adj','adj','noun']
-vow =['a','e','i','o','u']
+#ss = ['verb','adj','adj','noun']
+#vow =['a','e','i','o','u']
 
-	ans =ss.map do |req|
-	arc= vow.include?(req[0]) ? 'an' : 'a'
-print "Give me a #{arc} #{req}: "
-rep =gets.chomp 
+#	ans =ss.map do |req|
+#	arc= vow.include?(req[0]) ? 'an' : 'a'
+#print "Give me a #{arc} #{req}: "
+#rep =gets.chomp 
 
+#end
+
+
+#text = "I decided to #{ans[0]} a #{ans[1]} party for my #{ans[2]} #{ans[3]}."
+#puts text
+
+
+
+#Pig_latin
+
+
+	vow = ['a','e','i','o','u']
+  def pig_vow(string="")
+	string.each_char do |char|
+	if vow.include.index(char)
+		return string.index(char)
+	end
+	end
+	nil
+	end
+
+def vow(array=[])
+  array.find_index {|char| vow.include?(char) }
+end
+
+     def pig_latin(word="")
+	word.downcase!
+	lst = word.split('')
+	pos= vow(lst)
+	if pos>0
+		fst= lst[0...pos]
+		sec = lst [pos..-1]
+	else
+	fst =[]
+	sec=lst
+	end
+	fst.join+sec.join+'ay'
+	
+end
+
+def remove_punctuation(word="")
+  punctuation = '.,;:!?'.split('')
+  if punctuation.include?(word[-1,1])
+    word.chop!
+  end
+  word
+end
+
+def sentence_to_pig_latin(sentence="")
+  words = sentence.split(' ')
+  array = words.map do |word|
+    word_no_punct = remove_punctuation(word)
+    pig_latin(word_no_punct)
+  end
+  array.join(' ').capitalize + '.'
 end
 
 
-text = "I decided to #{ans[0]} a #{ans[1]} party for my #{ans[2]} #{ans[3]}."
-puts text 
+ puts "Type phrases to translate"
+  loop do
+    print "translate: "
+    input = gets.chomp
+    if 
+      puts sentence_to_pig_latin(input)
+      puts
+    end
+  end 

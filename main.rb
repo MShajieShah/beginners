f8d6e8f0f26231b949a0dd0d02c746da2af48adc
shajie <shajieshah@gmail.com>
2020-04-08 21:21:42 +0500

    Issues fixed

--- main.rb
+++ main.rb
@@ -24,8 +24,8 @@ def rang (a,b)
 
 end
 
-print rang(5,3) ,"\n"
-print rang(0,2) ,"\n"
+puts rang(5,3)
+puts rang(0,2)
 
 
 # => Problem #9
@@ -35,28 +35,28 @@ def rang (a,b,c)
 
 end
 
-print rang(1,31,41) ,"\n"
-print rang(3,6,10) ,"\n"
+puts rang(1,31,41)
+puts rang(3,6,10)
 
 
 # => Problem #15
 #Write a Ruby program to create a new string from a given string where the first and last characters have been exchanged.
 
-def charc(t)
+def revert(t)
     t[-1] << t[1...-1] << t[0]
 end
-print charc("abc"),"\n"
+puts revert("abc")
 
 
 # => Problem #24
 #Write a Ruby program to create a new string from a given string with the last character added at the front and back of the given string. The length of the given string must be 1 or more.
 
-def charc(l)
+def temp(l)
     l[-1] + l + l[-1]
 end
-print charc("abc"),"\n"
-print charc("abcd"),"\n"
-print charc("java")
+puts temp("abc")
+puts temp("abcd")
+puts temp("java")
 
 
 # => Problem #27
@@ -67,7 +67,7 @@ print "Even numbers between 2 to 10: ", "\n"
 a.each do |x| 
   if x % 2 == 0  
     puts x
-  end
+end
 end
 
 
@@ -85,13 +85,13 @@ end
 #Write a Ruby program to retrieve the total marks where subject name and marks of a student stored in a hash. Go to the editor
 #Sample subject and marks : Literature -74, Science – 89, Math-91//
 
-    subj = {
-        'lit' => 74 ,
-        'Sci' => 89 ,
-        'mat' => 91 ,
-        
-    }
-    
+subj = {
+    'lit' => 74 ,
+    'Sci' => 89 ,
+    'mat' => 91 ,
+
+}
+
 res = subj.values.inject { |key, values| key + values }
 puts  "Total Marks: " + res.to_s
 
@@ -104,10 +104,10 @@ def leap?(year)
   (year % 4 == 0)
 end
 
-print leap?(2012), "\n"
-print leap?(1500), "\n"
-print leap?(1600), "\n"
-print leap?(2020), "\n"
+puts leap?(2012)
+puts leap?(1500)
+puts leap?(1600)
+puts leap?(2020)
 
 # => Problem #35
 #Write a Ruby program to create a string using the first two characters (if present) of a given string if the first character is 'p' and second one is 's' otherwise return a blank string
@@ -115,11 +115,12 @@ print leap?(2020), "\n"
 print "Enter char "
 char = gets.chomp
 
-    if char[0] == 'p' && char[1] == 's'
-        puts char
-    else 
-        return 0;
-    end
+if temp[0] == 'p' && temp[1] == 's'
+    print temp
+else 
+    return nil
+
+end
 
 
 # => Problem #36
@@ -131,25 +132,25 @@ def str (q,w)
     a= (10/q)
     b= (10/w)
 
-if a<b
-    puts q
-    
+    if a<b
+        puts q
+
     else
 
         if a>b
             puts w 
-        
+
         else a==b
-    	   return 0
+            return 0
 
         end
 
     end
 end
 
-print str(5,5), "\n"
-print str(5,9)
-print str(5,7)
+puts str(5,5)
+puts str(5,9)
+puts str(5,7)
 
 
 
@@ -158,12 +159,12 @@ print str(5,7)
 def array_count (arry)
     arry.count do |e|
         e == 5
-         
-        end
+
     end
-print array_count([1, 2, 9]),"\n"
-print array_count([1, 2, 5, 9]),"\n"
-print array_count([1, 2, 5, 9, 5]),"\n"
+end
+puts array_count([1, 2, 9])
+puts array_count([1, 2, 5, 9])
+puts array_count([1, 2, 5, 9, 5])
 
 # => Problem #42
 #Write a Ruby program to check whether one of the first 5 elements in a given array of integers is a 7. The array length may be less than 5.
@@ -171,12 +172,12 @@ print array_count([1, 2, 5, 9, 5]),"\n"
 def array_count (arry)
     arry.count do |e|
         e == 7
-         
-        end
+
     end
-print array_count([1, 2, 9]),"\n"
-print array_count([1, 2, 7, 9]),"\n"
-print array_count([1, 2, 7, 9, 7]),"\n"
+end
+puts array_count([1, 2, 9])
+puts array_count([1, 2, 7, 9])
+puts array_count([1, 2, 7, 9, 7])
 
 
 
@@ -191,20 +192,22 @@ def str (q,w)
 
     if a<b
         puts w
-    
-        else
 
-            if b<a
-                puts q 
-        
-            else a==b
-           return 0
+    else
 
-        end
+        if b<a
+            puts q 
 
-    end
+        else a==b
+         return 0
+
+     end
+
+ end
 end
 
-print str(9,12)
-print str(110,200)
-print str(10,10), "\n"
+puts str(9,12)
+puts str(110,200)
+puts str(10,10)
+
+

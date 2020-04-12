0cf0debd4ee709f07984298739b9e2ea76e03423
shajie <shajieshah@gmail.com>
2020-04-13 00:17:03 +0500

    Remainig Problem Exercise of Arrays

--- main.rb
+++ main.rb
@@ -46,7 +46,7 @@ print new_arr ,"\n"
 #Write a Ruby program to check two given arrays of integers and test whether they have the same first element or they have the same last element. Both arrays length must be 1 or more. 
 def check_first_and_last_digit?(a,b)
 	if a.length >= 1 || b.length >= 1
-		return (a[0] == b[0] || a[-1] == b[-1])
+		return (a[0] == b[0] || a[-1] == b[-1])	
 	else
 		return "Invalid Array"
 	end
@@ -58,12 +58,16 @@ puts check_first_and_last_digit?([1, 2, 3], [4, 2,3,5,6])
 
 # => Problem #8
 #Write a Ruby program to remove blank elements from a given array
-colors = ["Red", "Green", "", "Blue", "White",nil]
+colors = ["Red", "Green", "", "Blue", "White"]
 print "Original array :"
 puts colors
-print "Remove blanks from array","\n"
-colors.reject!{|a| a.to_s.empty? }
-puts colors
+if colors.include?(nil)
+	puts "Give me a full String"
+else
+	print "Remove blanks from array","\n"
+	colors.delete("")
+	puts colors
+end
 
 # => Problem #10
 # Write a Ruby program to split a delimited string into an array.
@@ -75,14 +79,13 @@ print n.split(",").map { |s| s.to_i }, "\n"
 
 # => Problem #11
 # Write a Ruby program to create an array with the elements "rotated left" of a given array of integers length 3
-def elements_rotated_left(arr)
+def array_rotation (arr)
 	if arr.length == 3
-		return arr[2],arr[1], arr[0]
+		return arr.rotate
 	else
-		puts "Invalid"	
+		print "Enter array smaller than length 3"
 	end
-end
-print elements_rotated_left([1,2,3]) , "\n"
+	print array_rotation([1,2,3]) , "\n"
 
 # => Problem #12
 #Write a Ruby program to create a new array with the elements in reverse order from a given an array of integers length 3.
@@ -114,21 +117,70 @@ print find_larger_number([1, 2, 5])
 # => Problem #15
 #Write a Ruby program to create an array of length 2 containing their middle elements from two given arrays of integers of length 3.
 
-def middle(a,b)
-	sorted = a.sort
-	sort = b.sort
-	list = sorted.length
-	if list %2 != 0
-		puts sorted[list/2]
-	else
-		print (sorted[list/2]+sorted[(list/2)-1])/2.0
-	end
-	list = sorted.length
-	if list %2 != 0
-		puts sorted[list/2]
-	else
-		print  (sorted[list/2]+sorted[(list/2)-1])/2.0
+def middle(first,second)
+	a = first.sort
+	b = second.sort
+	if (a.length) %2 != 0 || (b.length) %2 != 0
+		return [a[(a.length)/2] , b[(b.length)/2]];
 	end
 
 end
-print middle([1,2,4],[1,2,3])
+print middle([1,2,4,678,7],[1,2,3]) , "\n"
+
+
+# => Problem #16
+# Write a Ruby program to concatenate array of arrays into one.
+a=[1,2,3,[4,[5,6]]]
+print "Original Values: "
+print a ,"\n"
+print "Concatenate array: "
+print a.flatten! , "\n"
+
+
+# => Problem #19
+#Write a Ruby program to check whether a given array of integers contains 3 twice, or 5 twice. The array will be length 0, 1, or 2.
+
+def check_integers_3twice_5twice?(nums)
+	if(nums.length == 2)
+	(nums[0] == 3 && nums[1] == 3) || (nums[0] == 5 && nums[1] == 5)
+	end
+end
+
+puts check_integers_3twice_5twice?([1, 1]) 
+puts check_integers_3twice_5twice?([2, 2]) 
+puts check_integers_3twice_5twice?([3, 3]) 
+puts check_integers_3twice_5twice?([5, 5])   
+puts check_integers_3twice_5twice?([8, 8])
+
+
+# => Problem #28
+#Write a Ruby program to create a new array with the first element of two arrays. If length of any array is 0, ignore that array.
+def check_array(a, b)
+	front = []
+	if(a.length > 0 && b.length > 0)
+		return front = a[0] , b[0]
+	else (a.length < 0 && b.length < 0)
+		front = a[0],b[0]
+		return front.compact
+	end
+end
+
+print check_array([3, 4, 5, 6], [7, 3, 4]),"\n"
+print check_array([3, 4, 5], [6, 7, 3, 4, 7]),"\n"
+print check_array([3, 4], []), "\n"
+
+
+# => Problem #30 
+# Write a Ruby program to find the difference between the largest and smallest values of a given array of integers and length 1 or more.
+def diff_between_largest_and_smallest_value(arr)
+	if arr.length > 1
+		a = arr.max
+		b = arr.min
+		return (a-b)
+	else
+		puts "Enter array greater than length 1"
+	end
+end
+puts diff_between_largest_and_smallest_value [1,2,3]
+puts diff_between_largest_and_smallest_value []
+

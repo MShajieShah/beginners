409e6ccbb832a6bbff9848d768eddf9bf68f19cd
shajie <shajieshah@gmail.com>
2020-05-04 23:07:38 +0500

    Concate problem solved

--- Mixedexercises.rb
+++ Mixedexercises.rb
@@ -8,6 +8,11 @@ print a.select{ |e| a.count(e) > 1 }.uniq ,"\n"
 # Given an array of characters & one word, find out if the word can be made from these characters.
 gues = %w(e n g a r o e)
 word = "orange"
-
-#All guesses in @word?:
 puts word.chars.all?{|char| gues.include?(char)}
+
+# => problem#6 
+# concatenation
+def concat(a)
+ a.flatten! , "\n"
+end
+print concat [[1], [2], [3], [4], [5], [6], [7]]

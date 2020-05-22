b55cd25baa558fff5dbd6a5f1dc0a014e4b86fc4
shajie <shajieshah@gmail.com>
2020-05-22 22:48:57 +0500

    Challenges classes_and_modules

---
+++ ruby_project/Classes/animal.rb
@@ -0,0 +1,18 @@
+class Animal
+
+	attr_accessor :noise , :color
+
+	#same As =>
+	# attr_reader :noise
+	# attr_writer :noise
+    
+
+	# def make_noise
+	# @noise
+	# end
+	
+	# def set_noise       #noise = value
+	# @noise = 'Onik'      #@noise = value
+	# end
+
+end

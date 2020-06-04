25a7e6cecdbf180afb87472ec84a8e297246568c
shajie <shajieshah@gmail.com>
2020-06-04 22:23:06 +0500

    Spoonerism file

---
+++ spoonerism.rb
@@ -0,0 +1,4 @@
+def check_vowels(str)
+  str.sub(/([^aeiou]*)(\w*) ([^aeiou]*)(\w*)/, '\3\2 \1\4')      
+end							# put ^ at the beginning to indicate that the following range [^aeiou] ,	
+puts check_vowels("loud noise")

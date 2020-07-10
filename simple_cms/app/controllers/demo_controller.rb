e57f3e8d2b986f44574724ab5f95bc9a1533bfe4
shajie <shajieshah@gmail.com>
2020-07-10 09:16:29 +0500

    challenge dynamic template

--- simple_cms/app/controllers/demo_controller.rb
+++ simple_cms/app/controllers/demo_controller.rb
@@ -15,4 +15,19 @@ class DemoController < ApplicationController
     # redirect_to(:controller => 'demo' , :action => 'index')
   end
 
+  def about
+     render('about_us')
+   end
+
+   def contact
+     if ['us', 'ca'].include?(params[:country])
+       @phone = '(800) 555-6789'
+     elsif params[:country] == 'uk'
+       @phone = '(020) 7946 1234'
+     else
+       @phone = '+1 (987) 654-3210'
+     end
+     render('contact_us')
+  end
+
 end
--- simple_cms/config/routes.rb
+++ simple_cms/config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
   root 'demo#index'
+  get 'demo/contact_us'
+  get 'demo/about_us'
   get 'demo/index'
   get 'demo/hello'
 
   #default route
   # get ':controller(/:action(/:id))'
-  
+
 end
